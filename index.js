a9861b40d694457994950453e199c5376c9165e6
Brandon <bjkrynicki@gmail.com>
2021-07-12 07:20:37 -0500

    commit tree 19a1aca5804ca70500ade4f61037952dbb80e93b

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(drivers, name) {
+  const matchDrivers = drivers.filter(function (person) {
+    return person.toLowerCase() === name.toLowerCase();
+  });
+  return matchDrivers;
+}
+
+function fuzzyMatch(source, testString) {
+  return source.filter(
+    (possibleMatch) =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+  );
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(function (driver) {
+    return driver.name === name;
+  });
+}
